d89b00f6e63357baff1ea6be93d5af65e4985729
Shisiajoy <156714344+Shisiajoy@users.noreply.github.com>
2024-02-22 18:46:08 +0300

    Update highestprice.py
    for loop error correction

--- highestprice.py
+++ highestprice.py
@@ -2,8 +2,8 @@ def find_highest_priced_product(products):
     highest_price = float("-inf") #to start off initial price as the lowest price
     highest_product_name = None     #to start with no product name
 
-    for product,price in products:     #for loop to iterate tru each dictionary in the list
-        product_name = product["name"] #to extract names from each dictionary in the list
+    for product in products:     #for loop to iterate tru each dictionary in the list
+        product_name = str(product["name"]) #to extract names from each dictionary in the list
         product_price = float(product["price"])   #to extract prices from each dictionary in the list
         if product_price >= highest_price:  #>= to compare if the product price is higher or equal to the highest price
             highest_price = product_price     #replacing highest price with product price if condition true
